5f3518cb5a6ea0453b1a00c2029bd20dda2ebc3f
Gallay David <david.gallay@heig-vd.ch>
2020-01-14 14:15:15 +0100

    defining lib interface

---
+++ src/treasure.cpp
@@ -0,0 +1,33 @@
+#include "treasure.h"
+using namespace std;
+
+int getRandomInRange(int max, int min=0) {
+
+}
+
+Map getEmptyMap(size_t height, size_t width) {
+
+}
+
+bool addTreasure(Map& map, size_t height, size_t width) {
+
+}
+
+void addRandomTreasure(Map& map) {
+
+}
+
+void addLake(Map& map, size_t originX, size_t originY, size_t radius) {
+
+}
+void addRandomLake(Map& map) {
+
+}
+
+bool addStart(size_t& x, size_t&y) {
+
+}
+
+Map initWorld(size_t& x, size_t&y) {
+
+}
---
+++ src/treasure.h
@@ -0,0 +1,30 @@
+#ifndef TREASURE_H
+#define TREASURE_H
+
+#include <vector>
+
+enum MapState {
+    MS_EARTH,
+    MS_WATER,
+    MS_TREASURE
+}
+
+
+typedef std::vector<MapState> Axe;
+typedef std::vector<Axe> Map;
+
+int getRandomInRange(int max, int min=0);
+
+Map getEmptyMap(size_t height, size_t width);
+
+bool addTreasure(Map& map, size_t height, size_t width);
+void addRandomTreasure(Map& map);
+
+void addLake(Map& map, size_t originX, size_t originY, size_t radius);
+void addRandomLake(Map& map);
+
+bool addStart(size_t& x, size_t&y)
+
+Map initWorld(size_t& x, size_t&y);
+
+#endif // TREASURE_H
