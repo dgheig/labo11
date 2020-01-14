0302889d70d94692a772fedd751bf0badde2b448
unknown <yannick@schaufel.net>
2020-01-14 15:04:08 +0100

    fonctions getRandomInRange, getWidth, getHeight, getEmptyMap et initWorld crées et remplies

--- src/treasure.cpp
+++ src/treasure.cpp
@@ -1,12 +1,33 @@
 #include "treasure.h"
+#include <cstdlib>
+#include <ctime> //time()
+
 using namespace std;
 
-int getRandomInRange(int max, int min=0) {
+int getRandomInRange(int max, int min) {
+   static bool firstTime = true;
+   if (firstTime) {
+      srand(time(NULL));
+      firstTime = false;
+   }
 
+   if (min < max) {
+      return rand() % (max - min + 1) + min;
+   } else {
+      return rand() % (min - max + 1) + max;
+   }
+}
+
+int getHeight() {
+   return 100;
+}
+
+int getWidth() {
+   return 200;
 }
 
 Map getEmptyMap(size_t height, size_t width) {
-
+   return Map(height, Axe(width, MS_EARTH));
 }
 
 bool addTreasure(Map& map, size_t height, size_t width) {
@@ -20,6 +41,7 @@ void addRandomTreasure(Map& map) {
 void addLake(Map& map, size_t originX, size_t originY, size_t radius) {
 
 }
+
 void addRandomLake(Map& map) {
 
 }
@@ -29,5 +51,15 @@ bool addStart(size_t& x, size_t&y) {
 }
 
 Map initWorld(size_t& x, size_t&y) {
+   const int numberOfLake = 3;
 
+   Map world = getEmptyMap(getHeight(), getWidth());
+
+   for (int i = 0; i < numberOfLake; ++i) {
+      addRandomLake(world);
+   }
+   addRandomTreasure(world);
+   addRandomStart(world);
+   
+   return world;
 }
--- src/treasure.h
+++ src/treasure.h
@@ -1,19 +1,23 @@
 #ifndef TREASURE_H
 #define TREASURE_H
 
+#include <cstdlib>
 #include <vector>
 
 enum MapState {
     MS_EARTH,
     MS_WATER,
     MS_TREASURE
-}
+};
 
 
 typedef std::vector<MapState> Axe;
 typedef std::vector<Axe> Map;
 
-int getRandomInRange(int max, int min=0);
+int getRandomInRange(int max, int min = 0);
+
+int getHeight();
+int getWidth();
 
 Map getEmptyMap(size_t height, size_t width);
 
@@ -23,8 +27,8 @@ void addRandomTreasure(Map& map);
 void addLake(Map& map, size_t originX, size_t originY, size_t radius);
 void addRandomLake(Map& map);
 
-bool addStart(size_t& x, size_t&y)
+bool addStart(size_t& x, size_t& y);
 
-Map initWorld(size_t& x, size_t&y);
+Map initWorld(size_t& x, size_t& y);
 
 #endif // TREASURE_H
