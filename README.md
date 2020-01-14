fbde241f065fa01878975688126400502d2df2d6
Gallay David <david.gallay@heig-vd.ch>
2020-01-14 13:11:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# labo11 - Matrice
+
+### Contributors
+* schaufelberger yannick
+* Gallay David
+
+### Language
+English
+
+## Naming Conventions
+* Variables:
+* Functions:
+* Constants: SCREAMING_SNAKE_CASE
---
+++ labo_10_comte_emmanuelle_gallay_david.cpp
@@ -0,0 +1,27 @@
+/*
+-----------------------------------------------------------------------------------
+Laboratoire : labo_11
+Fichier     : labo_11_schaufelberger_yannick_gallay_david.cpp
+Auteur(s)   : yannick schaufelberger et David Gallay
+Date        :
+
+But         :
+Remarque(s) :
+
+                There is the github repository:
+                https://github.com/dgheig/labo11
+
+Compilateur : MinGW-g++ 6.3.0 and g++ 7.4.0
+-----------------------------------------------------------------------------------*/
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+#define WAIT_ENTER while(cin.get()!='\n')
+
+int main() {
+
+    WAIT_ENTER;
+    return EXIT_SUCCESS;
+}
